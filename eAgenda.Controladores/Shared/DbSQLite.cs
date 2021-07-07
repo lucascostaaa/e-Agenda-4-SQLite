f49e334de592c9b49dc203c6db45c2daf838cfbc
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-07-06 23:33:37 -0300

    Add project files.

---
+++ eAgenda.Controladores/Shared/DbSQLite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using System.Data.SQLite;
+
+
+namespace eAgenda.Controladores.Shared
+{
+    public static class DbSQLite
+    {
+
+    }
+}
